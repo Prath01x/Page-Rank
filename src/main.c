a41269a385a1bb6afe2e8a085e4ce1dc2270bd0b
Pratham Malhotra <prma00001@stud.uni-saarland.de>
2024-05-31 05:37:53 +0200

    love

--- src/main.c
+++ src/main.c
@@ -9,10 +9,38 @@
 
 #include "utils.h"
 
-int main(int argc, char *const *argv) {
-  // initialize the random number generator
-  rand_init();
+#include <stdlib.h>
+#include <getopt.h>
 
-  // TODO: Implement me
-  exit(0);
+void print_usage() {
+    printf("Usage: ./pagerank [OPTIONS]... [FILENAME]\n");
+    printf("Perform pagerank computations for a given file in the DOT format\n");
+    printf("  -h        Print a brief overview of the available command line parameters\n");
+    printf("  -r N      Simulate N steps of the random surfer and output the result\n");
+    printf("  -m N      Simulate N steps of the Markov chain and output the result\n");
+    printf("  -s        Compute and print the statistics of the graph as defined in section 3.4\n");
+    printf("  -p P      Set the parameter p to P%%. (Default: P = 10)\n");
 }
+
+int main(int argc, char *argv[]) {
+    int opt;
+    void rand_init();
+    while ((opt = getopt(argc, argv, "hr:m:sp:")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_usage();
+                exit(EXIT_SUCCESS);
+            // Handle other flags here
+            default:
+                fprintf(stderr, "Usage: %s [-h] [-r N] [-m N] [-s] [-p P] [FILENAME]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+
+  // initialize the random number generator
+ 
+
+
